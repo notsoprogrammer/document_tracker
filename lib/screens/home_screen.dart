492946e723901ff7641a33e8b17899e84fdd3b8f
notsoprogrammer <100104099+notsoprogrammer@users.noreply.github.com>
2026-01-09 09:27:57 +0800

    remove notes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -94,7 +94,7 @@ class _HomeScreenState extends State<HomeScreen> {
       documents.add(doc);
       // Add initial history entry
       // Include office and personnel snapshot so creation history doesn't change later
-      doc.addHistoryEntry('Document created', doc.person, notes: "${doc.fromOrTo} - ${doc.assignedTo}");
+      doc.addHistoryEntry('Document Received', doc.person);
     });
   }
 
